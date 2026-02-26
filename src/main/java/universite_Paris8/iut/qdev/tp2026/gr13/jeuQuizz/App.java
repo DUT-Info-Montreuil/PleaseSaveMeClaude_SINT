5997da203daf062309dbcb0834474cfef67b6e44
wael <bm06.wael@gmail.com>
2026-02-26 10:23:03 +0100

    creation projet

---
+++ src/main/java/universite_Paris8/iut/qdev/tp2026/gr13/jeuQuizz/App.java
@@ -0,0 +1,10 @@
+package universite_Paris8.iut.qdev.tp2026.gr13.jeuQuizz;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/universite_Paris8/iut/qdev/tp2026/gr13/jeuQuizz/AppTest.java
@@ -0,0 +1,19 @@
+package universite_Paris8.iut.qdev.tp2026.gr13.jeuQuizz;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
