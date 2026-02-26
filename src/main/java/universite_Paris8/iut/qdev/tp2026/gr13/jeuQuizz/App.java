cfa3240f541adde262400dc5a30f35016cb2ca33
wael <bm06.wael@gmail.com>
2026-02-26 10:49:01 +0100

    architecture des packages

--- src/main/java/universite_Paris8/iut/qdev/tp2026/gr13/jeuQuizz/App.java
+++ src/main/java/universite_Paris8/iut/qdev/tp2026/gr13/jeuQuizz/App.java
@@ -5,6 +5,6 @@ package universite_Paris8.iut.qdev.tp2026.gr13.jeuQuizz;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Wael!");
     }
 }
